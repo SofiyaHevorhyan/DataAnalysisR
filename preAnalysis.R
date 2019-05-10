af827bcdc36f95585927649d1dbc754c704d90df
Iryna Popovych <32884361+ipopovych@users.noreply.github.com>
2019-05-10 11:07:48 +0300

    Delete preAnalysis.R

--- preAnalysis.R
+++
@@ -1,9 +0,0 @@
-startup<- read.csv(file="./data/CAX_Startup_Data.csv", header=TRUE,as.is=T)
-
-# replacing 'No Info' and 'blanks' with NA
-startup[startup=="No Info"]<- NA
-startup[startup==""]<- NA
-
-# converting column as date
-startup$Est..Founding.Date <- as.Date(startup$Est..Founding.Date, "%m/%d/%Y")
-startup$Last.Funding.Date <- as.Date(startup$Last.Funding.Date, "%m/%d/%Y")
